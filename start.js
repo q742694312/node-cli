1e1099b8b41017da5ed3afd6bf2cc4a8fb6ed2ee
振军 乔 <742694312@qq.com>
2022-03-04 15:38:06 +0800

    fix:84->bd

--- start.js
+++ start.js
@@ -1,11 +1,14 @@
-import { Wgs84toShanghai } from './Wgs84toShangHaiConvertor.js'
+import { Wgs84toShanghai,ShanghaiToWgs84 } from './Wgs84toShangHaiConvertor.js'
+import { bd09togcj02,gcj02tobd09, wgs84togcj02, gcj02towgs84} from './coordtransform.js'
 import * as xlsx from 'node-xlsx'
 import * as fs from 'fs'
 import * as path from 'path'
 //sheetjs.com/
 
-var sheets = xlsx.parse('./123.xlsx');
+var sheets = xlsx.parse('./xlsx/20220304.xlsx');
 
+var x_index=3
+var y_index=4
 // let sheets = xlsx.parse(path.join(__dirname, 'browser.xlsx'))
 // const workSheetsFromBuffer = xlsx.parse(fs.readFileSync(`${__dirname}/南大宝山树木数据.xlsx`));
 
@@ -16,9 +19,12 @@ for (var i in excelObj) {
 	var arr = [];
 	var line = excelObj[i];
 	if(i>0){
-		let wgs84_X=line[2]
-		let wgs84_Y=line[3]
-		let {X,Y}=Wgs84toShanghai([wgs84_Y,wgs84_X])
+		let wgs84_X=line[x_index]
+		let wgs84_Y=line[y_index]
+		// let {X,Y}=ShanghaiToWgs84([wgs84_Y,wgs84_X])
+	
+		let {X,Y}=WGS84_bd(wgs84_X,wgs84_Y)
+
 		line.push(X)
 		line.push(Y)
 	}
@@ -33,6 +39,16 @@ var buffer = xlsx.build([
 	}
 ]);
 
+
+
+function WGS84_bd(wgs84_X,wgs84_Y) {
+	let [g_x,g_y]=wgs84togcj02(wgs84_X,wgs84_Y)
+	let [B_X,B_y]=	gcj02tobd09(g_x,g_y)
+	return {
+		X:B_X,
+		Y:B_y
+	}
+}
 
 //将文件内容插入新的文件中
 fs.writeFileSync('./test1.xlsx', buffer, { 'flag': 'w' });
